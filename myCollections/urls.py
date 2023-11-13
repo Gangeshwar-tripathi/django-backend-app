4a6aa10763565b20a24db8b93dd4a6a369b0c095
Gangeshwar-tripathi <gangeshwar.tripathi22@gmail.com>
2023-11-13 19:17:55 +0530

    trailing slash in collection url is fixed

--- myCollections/urls.py
+++ myCollections/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('movies/', MoviesListView.as_view(), name='movies'),
     path('collection/', CollectionListView.as_view(), name='collection'),
-    path('collection/<slug:collection_uuid>', CollectionListDetailView.as_view(), name='detail-collection'),
+    path('collection/<slug:collection_uuid>/', CollectionListView.as_view(), name='detail-collection'),
     path('request-count/', RequestCountView.as_view(), name='request-count'),
     path('request-count/reset/', RequestCountView.as_view(), name='request-count-reset'),
 ]
